4852af1a1321dfd7452920c42f04bdfbe539b876
hazardoushazik <hazik.ahmad96@gmail.com>
2019-01-17 22:18:24 -0500

    fix congfig.js

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,12 +5,7 @@ const req = require.context( '../src', true, /.stories.js$/);
 
 function loadStories () {
   req.keys().forEach ( filename => req ( filename ));
-}
-
-configure ( loadStories , module );
-
-function loadStories() {
   require( '../src/stories' );
 }
 
-configure(loadStories, module);
+configure ( loadStories , module );
